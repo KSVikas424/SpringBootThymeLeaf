f790bfd91f8f218d815f41d884d22ca331224666
ksvikas424 <srinivasvikask@gmail.com>
2021-05-04 11:18:44 +0530

    initial commit

---
+++ SpringBoot-ThymeLeaf-1/src/test/java/com/example/demorest/SpringBootThymeLeaf1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeLeaf1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
